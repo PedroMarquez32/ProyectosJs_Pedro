3cf83a5c9884dceaec937c010f694d52dd1dfdd0
Destructordead32 <pedrojaviermarquelizana@gmail.como>
2025-02-23 02:36:22 +0100

    Proyecto Final

--- ProyectoCine_Bien_Entrega/frontend/src/router/index.jsx
+++ ProyectoCine_Bien_Entrega/frontend/src/router/index.jsx
@@ -27,7 +27,11 @@ export const router = createBrowserRouter([
       },
       {
         path: "movie/:id",
-        element: <MovieDetail />,
+        element: (
+          <ProtectedRoute>
+            <MovieDetail />
+          </ProtectedRoute>
+        ),
       },
       {
         path: "login",
